4909ca79dcaaba02a5f7a81153de62f7910c60fd
henrique <henrique01022008@gmail.com>
2026-01-13 11:25:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRmhL8hBgWNEZQ9_-rnyr5GSph5ixlpmC4kZAln-wFxIXjCSFySGTbUa0BfpSV3DoScj57e2oCR-wDK/pub?output=csv")
   .then(res => res.text())
-  .then(texto => {
-    const linhas = texto.trim().split("\n").slice(1);
-    linhas.forEach(linha => {
-      const parts = linha.includes(";") ? linha.split(";") : linha.split(",");
-      const [id, nome, kg, un, dz, cx, ativo] = parts;
+ linhas.forEach(l => {
+  const parts = l.includes(";") ? l.split(";") : l.split(",");
+  let [id, nome, kg, un, dz, cx, ativo] = parts;
 
-      if (!ativo || ativo.trim().toLowerCase() !== "sim") return;
+  if (!id || !nome) return;
+  if (!ativo || ativo.trim().toLowerCase() !== "sim") return;
 
+  id = id.trim().toLowerCase();
+  nome = nome.trim();
 
-      produtos[id] = {
-        nome,
-        precos: {}
-      };
-      if (kg) produtos[id].precos.Kg = Number(kg);
-      if (un) produtos[id].precos.Un = Number(un);
-      if (dz) produtos[id].precos.Dz = Number(dz);
-      if (cx) produtos[id].precos.Cx = Number(cx);
-    });
-    renderProdutos();
-  })
-  .catch(err => {
-    document.getElementById("produtos").innerHTML =
-      "<p style='color:red;font-weight:bold'>Erro ao carregar produtos</p>";
-  });
+  produtos[id] = {
+    nome,
+    precos: {}
+  };
 
+  if (kg) produtos[id].precos.Kg = Number(kg.replace(",", "."));
+  if (un) produtos[id].precos.Un = Number(un.replace(",", "."));
+  if (dz) produtos[id].precos.Dz = Number(dz.replace(",", "."));
+  if (cx) produtos[id].precos.Cx = Number(cx.replace(",", "."));
+});
