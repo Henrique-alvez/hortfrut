2c43420d3fbfd55dcf0547d3319551ebc34a92e5
henrique <henrique01022008@gmail.com>
2026-01-13 11:17:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
-let produtos = {};
-let carrinho = {};
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRmhL8hBgWNEZQ9_-rnyr5GSph5ixlpmC4kZAln-wFxIXjCSFySGTbUa0BfpSV3DoScj57e2oCR-wDK/pub?output=csv")
+  .then(res => res.text())
+  .then(texto => {
+    const linhas = texto.trim().split("\n").slice(1);
+    linhas.forEach(linha => {
+      const parts = linha.includes(";") ? linha.split(";") : linha.split(",");
+      const [id, nome, kg, un, dz, cx, ativo] = parts;
 
-const API_KEY = "AIzaSyC54WUG4EqN3mFkYuEhpZbrzQoKatFf4pc";
-const SHEET_ID = "1lKBi3XyQ0EqvXjU42w9vz_5AnNKJKrFvGQYOCRPPkWQ";
-const RANGE = "produtos!A2:G";
-
-const url = `https://sheets.googleapis.com/v4/spreadsheets/1lKBi3XyQ0EqvXjU42w9vz_5AnNKJKrFvGQYOCRPPkWQ/values/produtos!A2:G?key=AIzaSyC54WUG4EqN3mFkYuEhpZbrzQoKatFf4pc
-
-
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    if (!data.values) throw new Error("Planilha vazia");
-
-    data.values.forEach(linha => {
-      const [id, nome, kg, un, dz, cx, ativo] = linha;
-
-      if (!id || ativo !== "sim") return;
+      if (ativo !== "sim") return;
 
       produtos[id] = {
         nome,
         precos: {}
       };
-
       if (kg) produtos[id].precos.Kg = Number(kg);
       if (un) produtos[id].precos.Un = Number(un);
       if (dz) produtos[id].precos.Dz = Number(dz);
       if (cx) produtos[id].precos.Cx = Number(cx);
     });
-
     renderProdutos();
   })
   .catch(err => {
-    console.error(err);
     document.getElementById("produtos").innerHTML =
-      "<p style='color:red'>Erro ao carregar produtos</p>";
+      "<p style='color:red;font-weight:bold'>Erro ao carregar produtos</p>";
   });
-
